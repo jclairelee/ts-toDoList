d6c6f9dfcaa92f968906de25046835934ca82d4b
Claire Lee <jclairelee@gmail.com>
2023-02-24 13:45:56 -0800

    make createDOMelements function

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,41 @@
 import "./style.css";
 
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("userinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
-const list = document.getElementById("list")!;
 
-const submithandler = (e: SubmitEvent) => {
-  e.preventDefault();
-  const newTodo = input.value;
+//plan = arr of Todos
+let plan: Todo[] = [];
+
+const createDOMelements = (item: Todo) => {
   const newTodoBox = document.createElement("div");
   newTodoBox.id = "newTodoBox";
   form.appendChild(newTodoBox);
-
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   newTodoBox.appendChild(checkbox);
-
   const newTask = document.createElement("label");
-  newTask.innerHTML = newTodo;
+  newTask.innerHTML = item.task;
   newTodoBox.appendChild(newTask);
+};
+
+const submithandler = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  //newTaskObj is created, which will be appended into the array of plan
+  const newTaskObj: Todo = {
+    task: input.value,
+    completed: false,
+  };
+  createDOMelements(newTaskObj);
+  plan.push(newTaskObj);
+  console.log(plan);
+
   input.value = "";
 };
 
