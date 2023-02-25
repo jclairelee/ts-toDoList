34992e8f92b96be9e70a16035f24b229d47153ac
Claire Lee <jclairelee@gmail.com>
2023-02-24 22:35:23 -0800

    update date function and readme.md

---
+++ README.md
@@ -0,0 +1,35 @@
+# To-Do-List
+
+typescript mini project, to-do-list holds a list of tasks that needs to be completed.
+
+## Screenshot
+
+![Screenshot](./public/miniTodos.gif)
+
+## Key Features
+
+- Add new tasks
+- Delete tasks
+- Mark tasks as completed
+- Save tasks in LocalStorage
+
+## Tech
+
+- Typescript
+- Sass
+- DOM manipulation
+- Vite
+
+## Installation
+
+1. Install dependencies
+
+```bash
+  npm i
+```
+
+2. Run the code
+
+```bash
+  npm run dev
+```
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 
 interface Todo {
   id: number;
@@ -11,8 +11,63 @@ const form = document.querySelector("form")!;
 
 //plan = arr of Todos
 const plan: Todo[] = getLocalStorageItems();
+
+const todaysDate = document.querySelector("h1")! as HTMLHeadingElement;
+todaysDate.innerHTML = getDate();
+
+const totalnum = document.querySelector("h2")! as HTMLHeadingElement;
+totalnum.innerHTML = `${plan.length} Tasks`;
+
 //display every obj that are stored in the arr named plan
 plan.forEach(createDOMelements);
+
+function getDate() {
+  let date = new Date();
+  let month: string;
+  switch (date.getMonth()) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+    default:
+      month = "not found!";
+  }
+  let day = date.getDate();
+  let yr = date.getFullYear();
+  return `${month} ${day}, ${yr}`;
+}
 function getLocalStorageItems(): Todo[] {
   const planJSON = localStorage.getItem("todos");
   if (planJSON === null) {
@@ -23,6 +78,8 @@ function getLocalStorageItems(): Todo[] {
 }
 function updateStatus() {
   localStorage.setItem("todos", JSON.stringify(plan));
+  totalnum.innerHTML = `${plan.length} Tasks`;
+  window.location.reload();
 }
 function removeOldList(
   elToDelete: HTMLDivElement,
@@ -41,6 +98,8 @@ function removeOldList(
     });
     console.log(result);
     localStorage.setItem("todos", JSON.stringify(result));
+    totalnum.innerHTML = `${result.length} Tasks`;
+    window.location.reload();
   }
 }
 function createDOMelements(item: Todo) {
@@ -49,20 +108,20 @@ function createDOMelements(item: Todo) {
   const newTask = document.createElement("label");
   const deleteOldTask = document.createElement("img");
 
-  newTodoBox.className = "newTodoBox";
+  newTodoBox.className = "todos__newTodoBox";
   form.appendChild(newTodoBox);
 
   checkbox.type = "checkbox";
-  checkbox.className = "checkbox";
+  checkbox.className = "todos__checkbox";
   newTodoBox.appendChild(checkbox);
   checkbox.checked = item.completed;
 
-  newTask.className = "text-list";
+  newTask.className = "todos__textlist";
   newTask.innerHTML = item.task;
   newTodoBox.appendChild(newTask);
 
   deleteOldTask.src = "/delete.png";
-  deleteOldTask.className = "delete";
+  deleteOldTask.className = "todos__delete";
   newTodoBox.appendChild(deleteOldTask);
 
   deleteOldTask.addEventListener("click", () =>
